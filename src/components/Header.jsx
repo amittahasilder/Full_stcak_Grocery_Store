53d68b440126c28c8f1056792b58ad768856be4d
amittahasilder <amittahasilder@gmail.com>
2026-05-10 10:44:26 -0700

    your message

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
@@ -6,13 +7,19 @@ const Header = () => {
     <header>
       <div className='container flex items-center justify-between'>
         <div className='logo'>
-          <Image
+            <Link href={"/"}>
+            <Image
             src="/logo1.png"
-            width={243}
-            height={73}
+            width={233}
+            height={63}
             alt="Logo"
           />
+            </Link>
+          
         </div>
+       
+       
+
       </div>
     </header>
   )
---
+++ src/components/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div className='search bg-[#E6E6E6] w-[600px] h-[60px]'>
+
+    </div>
+  )
+}
+
+export default Search
